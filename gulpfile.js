a0e941ca51e3453ef2b9a0dc58b47d593f9c4403
cs <bartus.csongor@gmail.com>
2014-11-12 12:25:58 +0200

    Ready to roll on

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,17 @@ var gulp = require('gulp'),
     del = require('del'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
-    
+
     browserSync = require('browser-sync'),
 	reload = browserSync.reload,
-    
+
     swig = require('gulp-swig'),
     data = require('gulp-data'),
     fm = require('front-matter'),
-    
+
     minifycss = require('gulp-minify-css'),
     uglify = require('gulp-uglify');
-    
+
 
 
 function errorHandler(error) {
@@ -21,15 +21,27 @@ function errorHandler(error) {
 }
 
 
-// HTML
-gulp.task('html', function() {
-  return gulp.src('components/**/*.html')
+// Swig
+// - compiles a .swig file with YAML front matter
+// - renames to .html in the same folder
+gulp.task('swig', function() {
+  return gulp.src('components/**/*.swig')
     .pipe(data(function(file) {
       var content = fm(String(file.contents));
       file.contents = new Buffer(content.body);
       return content.attributes;
     }))
     .pipe(swig())
+    .pipe(rename({ extname: '.html' }))
+    .pipe(gulp.dest('components'))
+    .pipe(notify("Swig OK"));
+});
+
+
+// HTML
+// - moves all .html files from components/pages to build/
+gulp.task('html', function() {
+  return gulp.src('components/pages/*.html')
     .pipe(gulp.dest('build'))
     .pipe(notify("HTML OK"));
 });
@@ -65,30 +77,33 @@ gulp.task('clean', function(cb) {
 
 // Default task
 gulp.task('default', ['clean'], function() {
-  gulp.start('html', 'styles', 'scripts');
+  gulp.start('swig', 'html', 'styles', 'scripts');
 });
 
 
 // Watch
 gulp.task('watch', function() {
-  
-  // Watch .liquid files
-  gulp.watch('components/**/.liquid', ['html']);
-  
+
+  // Watch swig files
+  gulp.watch('components/**/.swig', ['swig']);
+
+  // Move compiled html
+  gulp.watch('components/pages/*.html', ['html']);
+
   // Watch .scss files
   gulp.watch('components/.css', ['styles']);
- 
+
   // Watch .js files
   gulp.watch('components/**/*.js', ['scripts']);
- 
+
   browserSync({
     notify: false,
     server: {
       baseDir: ['build']
     }
   });
- 
+
   // Watch any files in dist/, reload on change
   gulp.watch(['dist/**']).on('change', reload);
- 
+
 });
